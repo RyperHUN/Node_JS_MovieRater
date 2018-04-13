1b61184df6795c79ba6e9b17705b7fff44f28778
Ryperhun <akormanyzsolt@gmail.com>
2018-04-13 18:42:48 +0200

    Getting data with concat.js works

--- middleware/concat.js
+++ middleware/concat.js
@@ -3,17 +3,27 @@ var requireOption = require('./common').requireOption;
 module.exports = function (objectrepository) {
 
     return function (req, res, next) {
-        res.tpl.concat = [
-            {
-                name : "Lorem ipsum",
-                rating : 8.5
-            },
-            {
-                name : "Matrix IV",
-                rating : 9.9
-            },
-        ]   
-        return next();
+        var Rating = require('../model/Rating');
+        var User = require('../model/Movie');
+        var Movie = require('../model/User');
+        Rating.find({}).populate('user').populate('movie')
+        .exec(function (err, result){
+            if(err) {
+                console.log(err);
+            }else {
+                data = [];
+                
+                console.log('Length:' + result.length);
+                result.forEach(function(elem) {
+                    if(elem.movie == null || elem.user == null)
+                        return;
+                    data.push({name : elem.movie.name, rating : elem.rating});
+                });
+                res.tpl.concat = data;
+                
+                next();
+            }
+        });
     };
 
 };
