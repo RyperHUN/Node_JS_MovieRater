0e89863c4030181b1c67d1fe34692479e3bede23
Ryperhun <akormanyzsolt@gmail.com>
2018-04-26 22:12:35 +0200

    Menu works perfectly, colors every menu according to the selected one

--- middleware/render.js
+++ middleware/render.js
@@ -3,6 +3,11 @@ var requireOption = require('./common').requireOption;
 module.exports = function (objectrepository, viewName) {
 
     return function (req, res, next) {
+        res.tpl.viewName = "/";
+        if(viewName != "index") {
+            res.tpl.viewName = res.tpl.viewName + viewName;
+        }
+        console.log(res.tpl.viewName)
         console.log('Rendering:' + viewName);
         res.render(viewName,res.tpl);
     };
