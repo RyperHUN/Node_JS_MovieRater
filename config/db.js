3281ba1b90534077cce05da049ba992eb5fa27da
Ryperhun <akormanyzsolt@gmail.com>
2018-04-26 20:07:26 +0200

    After every restart test data is added

--- config/db.js
+++ config/db.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', function(err){if(err) console.log(err)});
 mongoose.connection.once('open',function () {
   console.log('Connected');
+  var createTestData = require('./testData')
+  createTestData()
 }).on('error',function (error) {
   console.log('CONNECTION ERROR:',error);
 });
