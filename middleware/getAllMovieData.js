1ddcd2e739367e0b71c89b7fd00dd72d8f62766f
Ryperhun <akormanyzsolt@gmail.com>
2018-05-18 21:15:45 +0200

    Refactored getAllMovieData

--- middleware/getAllMovieData.js
+++ middleware/getAllMovieData.js
@@ -1,5 +1,13 @@
 var requireOption = require('./common').requireOption;
 
+var mapForObj = function(obj, func){
+    var newObj = {};
+    Object.keys(obj).map(function(key,index){
+        newObj[key] = func(obj[key]);
+    });
+    return newObj;
+}
+
 var mergeElements = function(result) {
     var data = {};
     result.forEach(function(elem) {
@@ -16,15 +24,15 @@ var mergeElements = function(result) {
                                 count : 1,  movieId : elem.movie._id};
         }
     });
-    var finalData = [];
-    for(var key in data) {
-        var averageRating =  data[key].rating / data[key].count;
+    var finalData = mapForObj(data, function(elem){
+        console.log(elem);
+        var averageRating =  elem.rating / elem.count;
         averageRating = Math.round(averageRating * 10) / 10;
-        var newObj = data[key];
-        delete newObj.rating;
-        newObj.rating = averageRating;
-        finalData.push(newObj);
-    }
+        delete elem.count;
+        elem.rating = averageRating;
+        return elem;
+    });
+    finalData = Object.values(finalData);
     return finalData;
 }
 
